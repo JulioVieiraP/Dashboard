908942db81fef434a815ae51877e7eab3065a9b6
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-19 18:57:53 -0300

    Feat: Card de novos clientes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign } from "lucide-react";
+import { DollarSign, Users } from "lucide-react";
 
 const Home = () => {
   return (
@@ -20,6 +20,24 @@ const Home = () => {
 
           <CardContent>
             <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">Novos Clientes</CardTitle>
+              <Users className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Novos Clientes em 30 dias
+            </CardDescription>
+
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">234</p>
           </CardContent>
         </Card>
 
