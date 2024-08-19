0ca4fde1114ad702c16f99d7599a95e775c7f173
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-19 18:45:43 -0300

    feat: criando o Card de Total de vendas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { DollarSign } from "lucide-react";
 
 const Home = () => {
   return (
     <main className="sm:ml-14 p-4">
-      <h1>Pojeto Dashboard</h1>
+      <section className="grid grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">Total vendas</CardTitle>
+              <DollarSign className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Total vendas em 90 dias
+            </CardDescription>
+
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+      </section>
     </main>
   );
 }
