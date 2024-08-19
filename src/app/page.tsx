759c5550e9dc416c9d353e497d6a3c3034d6d432
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-19 19:07:00 -0300

    Feat: Card total de pedidos hoje

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign, Users } from "lucide-react";
+import { DollarSign, Percent, Users } from "lucide-react";
 
 const Home = () => {
   return (
@@ -38,6 +38,24 @@ const Home = () => {
 
           <CardContent>
             <p className="text-base sm:text-lg font-bold">234</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">Pedidos hoje</CardTitle>
+              <Percent className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Total de pedidos hoje
+            </CardDescription>
+
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">65</p>
           </CardContent>
         </Card>
 
